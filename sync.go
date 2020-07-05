e8710a0221bd8e6790ee5481093fb9ca97e842e1
sanshirookazaki <sanshirookazaki@gmail.com>
2020-07-05 23:04:33 +0900

    sync once

--- sync.go
+++ sync.go
@@ -21,4 +21,19 @@ func main() {
 	}()
 
 	wait.Wait()
+
+	var once sync.Once
+	onceBody := func() {
+		fmt.Println("Only once")
+	}
+	done := make(chan bool)
+	for i := 0; i < 10; i++ {
+		go func() {
+			once.Do(onceBody)
+			done <- true
+		}()
+	}
+	for i := 0; i < 10; i++ {
+		<-done
+	}
 }
